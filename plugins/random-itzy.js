5cd03b1b9fdc07dbc6b267ada877dc09dcfdf92a
Katashi Fukushima <101235863+KatashiFukushima@users.noreply.github.com>
2024-05-04 15:28:04 -0500

    𝑱𝒐𝒂𝒏𝑩𝒐𝒕-𝑻𝑲

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 let handler = async(m, { conn, args, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/Itzy")
 let json = res.data
-let gata = json.url
-conn.sendFile(m.chat, gata, 'error.jpg', `_${command}_`, m)
-//conn.sendButton(m.chat, `_${command}_`, wm, gata, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
+let joan = json.url
+conn.sendFile(m.chat, joan, 'error.jpg', `_${command}_`, m)
+//conn.sendButton(m.chat, `_${command}_`, wm, joan, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 handler.help = ['itzy','kpopitzy']
 handler.tags = ['internet']

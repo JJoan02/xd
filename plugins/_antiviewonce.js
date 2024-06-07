a8cf22ad775ee75961468665204c5c09fa92926f
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-06-06 21:47:33 -0500

    𝑱𝒐𝒂𝒏𝑩𝒐𝒕-𝑻𝑲

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,40 +1,40 @@
-import { downloadContentFromMessage } from '@adiwajshing/baileys'
+import { downloadMediaMessage } from '@whiskeysockets/baileys';
 
-let handler = m => m
+let handler = m => m;
 handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
-    let media, msg, type
-    const { antiver, isBanned } = global.db.data.chats[m.chat]
-    if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
+    let media, msg, type;
+    const { antiver, isBanned } = global.db.data.chats[m.chat];
+    if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return;
     if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
-        msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message 
-        type = Object.keys(msg)[0]
+        msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message;
+        type = Object.keys(msg)[0];
         if (m.mtype == 'viewOnceMessageV2') {
-            media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio')
+            media = await downloadMediaMessage(msg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio');
         } else {
-            media = await downloadContentFromMessage(msg[type], 'audio')
+            media = await downloadMediaMessage(msg[type], 'audio');
         }
-        let buffer = Buffer.from([])
+        let buffer = Buffer.from([]);
         for await (const chunk of media) {
-            buffer = Buffer.concat([buffer, chunk])
+            buffer = Buffer.concat([buffer, chunk]);
         }
-        const fileSize = formatFileSize(msg[type].fileLength)
+        const fileSize = formatFileSize(msg[type].fileLength);
         const description = `
 🕵️‍♀️ *ANTI VER UNA VEZ* 🕵️\n
 🚫 *No ocultar* ${type === 'imageMessage' ? '`Imagen` 📷' : type === 'videoMessage' ? '`Vídeo` 🎥' : type === 'audioMessage' ? '`Mensaje de voz` 🔊' : 'este mensaje'}
 - *Tamaño:* \`${fileSize}\`
 - *Usuario:* *@${m.sender.split('@')[0]}*
-${msg[type].caption ? `- *Texto:* ${msg[type].caption}` : ''}`.trim()
-        if (/image|video/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
+${msg[type].caption ? `- *Texto:* ${msg[type].caption}` : ''}`.trim();
+        if (/image|video/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] });
         if (/audio/.test(type)) { 
-            await conn.reply(m.chat, description, m, { mentions: [m.sender] }) 
-            await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
+            await conn.reply(m.chat, description, m, { mentions: [m.sender] }); 
+            await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m });
         }
     }
-}
-export default handler
+};
+export default handler;
 
 function formatFileSize(bytes) {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-    return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i]
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY'];
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i];
 }

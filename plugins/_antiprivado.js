5b5c54aa413d8a3179fd1db245b0933a68501c4c
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-06-30 11:51:00 -0500

    𝑱𝒐𝒂𝒏𝑩𝒐𝒕-𝑻𝑲

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -2,36 +2,40 @@ const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--cod
 
 let handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, usedPrefix, command }) {
-if (m.isBaileys && m.fromMe) return !0
-if (m.isGroup) return !1
-if (!m.message) return !0
-const regex = new RegExp(`^${comandos.source}$`, 'i')
-if (regex.test(m.text.toLowerCase().trim())) return !0
+    if (m.isBaileys && m.fromMe) return !0 // Ignorar mensajes del propio bot
+    if (m.isGroup) return !1 // Ignorar mensajes en grupos
+    if (!m.message) return !0 // Ignorar mensajes sin contenido
 
-let chat, user, bot, mensaje
-chat = global.db.data.chats[m.chat]
-user = global.db.data.users[m.sender]
-bot = global.db.data.settings[this.user.jid] || {}
+    // Crear una expresión regular para los comandos permitidos
+    const regex = new RegExp(`^${comandos.source}$`, 'i')
+    
+    // Si el mensaje es privado y no es de un owner, verificar si es un comando permitido
+    if (!m.isGroup && !isOwner && !isROwner) {
+        if (!regex.test(m.text.toLowerCase().trim())) {
+            // Si el comando no está permitido, ignorar el mensaje
+            return !0
+        }
+    }
+    
+    // Código para manejo de antiPrivate
+    let chat, user, bot, mensaje
+    chat = global.db.data.chats[m.chat]
+    user = global.db.data.users[m.sender]
+    bot = global.db.data.settings[this.user.jid] || {}
 
-if (bot.antiPrivate && !isOwner && !isROwner) {
-if (user.counterPrivate === 0) {
-//mensaje = `*@${m.sender.split`@`[0]} 𝙀𝙎𝙏𝘼 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏.*\n\n⚠️ \`\`\`𝐏𝐑𝐈𝐌𝐄𝐑𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
-await conn.reply(m.chat, mid.smsprivado(m), m, { mentions: [m.sender] })  
-  
-} else if (user.counterPrivate === 1) {
-let grupos = redesMenu
-//mensaje = `*@${m.sender.split`@`[0]} 𝙎𝙀 𝘼 𝙈𝙀𝙉𝘾𝙄𝙊𝙉𝘼𝘿𝙊 𝘼𝙉𝙏𝙀𝙍𝙄𝙊𝙍𝙈𝙀𝙉𝙏𝙀 𝙌𝙐𝙀 𝙉𝙊 𝙎𝙀 𝙇𝙀 𝙋𝙐𝙀𝘿𝙀 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝘽𝙊𝙏 𝙀𝙉 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 🫤*\n\n👇 𝙋𝙐𝙀𝘿𝙀 𝙐𝙉𝙄𝙍𝙎𝙀 𝘼 𝙉𝙐𝙀𝙎𝙏𝙍𝙊 𝙂𝙍𝙐𝙋𝙊 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙀𝙇 𝘽𝙊𝙏\n\n${grupos}\n\n𝙎𝙄 𝙑𝙐𝙀𝙇𝙑𝙀 𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏,𝙎𝙀𝙍𝘼́ 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊‼️\n\n⚠️ \`\`\`𝐒𝐄𝐆𝐔𝐍𝐃𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
-await conn.reply(m.chat, mid.smsprivado1(m, grupos), m, { mentions: [m.sender] }) 
-  
-} else if (user.counterPrivate === 2) {
-//mensaje = `*@${m.sender.split`@`[0]} 𝙎𝙀𝙍𝘼́́ 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊(𝘼) 𝘿𝙀𝙇 𝘽𝙊𝙏. 😾 𝙎𝙀 𝙈𝙀𝙉𝘾𝙄𝙊𝙉𝙊́ 𝘼𝙉𝙏𝙀𝙍𝙄𝙊𝙍𝙈𝙀𝙉𝙏𝙀 𝙌𝙐𝙀 𝙉𝙊 𝙎𝙀 𝙋𝙊𝘿𝙄𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊.*\n\n⚠️ \`\`\`𝐓𝐄𝐑𝐂𝐄𝐑𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
-await conn.reply(m.chat, mid.smsprivado2(m), m, { mentions: [m.sender] }) 
-  
-user.counterPrivate = -1
-await this.updateBlockStatus(m.sender, 'block')
-}
-user.counterPrivate++
-}
-return !1
+    if (bot.antiPrivate && !isOwner && !isROwner) {
+        if (user.counterPrivate === 0) {
+            await conn.reply(m.chat, mid.smsprivado(m), m, { mentions: [m.sender] })  
+        } else if (user.counterPrivate === 1) {
+            let grupos = redesMenu
+            await conn.reply(m.chat, mid.smsprivado1(m, grupos), m, { mentions: [m.sender] }) 
+        } else if (user.counterPrivate === 2) {
+            await conn.reply(m.chat, mid.smsprivado2(m), m, { mentions: [m.sender] }) 
+            user.counterPrivate = -1
+            await this.updateBlockStatus(m.sender, 'block')
+        }
+        user.counterPrivate++
+    }
+    return !1
 }
 export default handler

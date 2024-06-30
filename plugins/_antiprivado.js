56821e78117e1352c06813a57c83727fd2bf2f7e
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-06-30 12:30:31 -0500

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,36 +1,23 @@
-const comandosPermitidos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i
+const comandos = /piedra|papel|tijera|estado|verificar|code|jadibot --code|--code|creadora|bottemporal|grupos|instalarbot|términos|bots|deletebot|eliminarsesion|serbot|verify|register|registrar|reg|reg1|nombre|name|nombre2|name2|edad|age|edad2|age2|genero|género|gender|identidad|pasatiempo|hobby|identify|finalizar|pas2|pas3|pas4|pas5|registroc|deletesesion|registror|jadibot/i;
 
-let handler = m => m
+let handler = m => m;
 handler.before = async function (m, { conn, isOwner, isROwner }) {
-    if (m.fromMe) return !0 // Ignorar mensajes enviados por el propio bot
-    if (m.isGroup) return !1 // Ignorar mensajes enviados en grupos
-    if (!m.message) return !0 // Ignorar mensajes que no contienen texto
+    if (m.fromMe) return true;
+    if (m.isGroup) return false;
+    if (!m.message) return true;
 
-    // Crear una expresión regular para los comandos permitidos
-    const regex = new RegExp(`^${comandosPermitidos.source}$`, 'i')
-    
-    // Obtener datos del usuario, chat y bot
-    let chat, user, bot
-    chat = global.db.data.chats[m.chat]
-    user = global.db.data.users[m.sender]
-    bot = global.db.data.settings[this.user.jid] || {}
+    const regexWithPrefix = new RegExp(`^${prefix.source}\\s?${comandos.source}`, 'i');
+    if (regexWithPrefix.test(m.text.toLowerCase().trim())) return true;
 
-    // Obtener lista de owners y oficiales
-    const owners = global.owner || []
-    const officials = global.official || []
-    const isAuthorized = owners.includes(m.sender) || officials.includes(m.sender)
+    let chat, user, bot, mensaje;
+    chat = global.db.data.chats[m.chat];
+    user = global.db.data.users[m.sender];
+    bot = global.db.data.settings[this.user.jid] || {};
 
-    // Verificar si el antiPrivate está activo
-    if (bot.antiPrivate) {
-        // Si el mensaje es privado y no es de un owner o un oficial, verificar si es un comando permitido
-        if (!m.isGroup && !isAuthorized) {
-            if (!regex.test(m.text.toLowerCase().trim())) {
-                // Si el comando no está permitido, ignorar el mensaje
-                return !0
-            }
-        }
+    if (bot.antiPrivate && !isOwner && !isROwner) {
+        return false;
     }
     
-    return !1 // No bloquear ni enviar advertencias, simplemente ignorar
-} 
-export default handler
+    return false;
+};
+export default handler;
